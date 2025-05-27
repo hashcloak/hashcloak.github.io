5c0165ed67c9f499d0b32a78c57c4446b7dfb1f8
Mikerah <mikerah@hashcloak.com>
2025-05-27 00:53:24 -0400

    Removed old page from routes

--- src/Components/Routes.tsx
+++ src/Components/Routes.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import HomePage from './Pages/HomePage';
 import Services from './Subpages/Services';
-import Contact from './Subpages/Contact';
 import Landing from './Subpages/Landing';
 
 const AppRoutes = () => {
@@ -11,7 +10,6 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/landing" element={<Landing />} />
       </Routes>
     </Router>
